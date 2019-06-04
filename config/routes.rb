5571ada42faeaec0d3d3a94e2e7d1c7660d6969c
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-04 14:00:46 +0200

    restaurant reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :restaurants, except: [:update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+  namespace :admin do
+    resources :restaurants, only: [:update, :destroy]
+  end
+  root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
